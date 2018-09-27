b22ad020cf68fe0e018050f1e0e005388e2d986d
Andrew Wei <andey.wei@gmail.com>
2018-09-27 11:07:04 -0400

    add the fetch button in admin

--- app/admin/stocks.rb
+++ app/admin/stocks.rb
@@ -1,6 +1,11 @@
 ActiveAdmin.register Stock do
   permit_params :ticker
 
+  member_action :fetch, method: :get
+  action_item :fetch, only: :show do
+    link_to 'fetch', admin_stock_path(resource)
+  end
+
   index pagination_total: false do
     column :updated_at
     column :ticker
@@ -8,5 +13,11 @@ ActiveAdmin.register Stock do
     column :price
     column :volume
     actions
+  end
+
+  controller do
+    def fetch
+      resource.fetch
+    end
   end
 end
